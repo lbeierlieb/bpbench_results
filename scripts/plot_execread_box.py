758d6f1ed9548529038c681729d66378b5e84753
Lukas Beierlieb <lukasbrlb.lb@gmail.com>
2025-01-16 13:44:39 +0100

    plots: change boxplots from ns to us/ms

--- scripts/plot_execread_box.py
+++ scripts/plot_execread_box.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-def read_files_and_plot_box(files, plot_filename, ylimit, textoffset, figsize, fontsize):
+def read_files_and_plot_box(files, plot_filename, ylimit, textoffset, figsize, fontsize, scaling, unit):
     data = []
     labels = []
 
     for filename, label in files:
         with open(filename, 'r') as f:
-            numbers = [float(line.strip()) for line in f if line.strip().isdigit()]
+            numbers = [float(line.strip()) / scaling for line in f if line.strip().isdigit()]
             if numbers:
                 data.append(numbers)
                 labels.append(label)
@@ -20,11 +20,11 @@ def read_files_and_plot_box(files, plot_filename, ylimit, textoffset, figsize, f
     plt.boxplot(data, labels=labels)
     for i, dataset in enumerate(data, 1):
         median = np.median(dataset)
-        plt.text(i+textoffset, median, f'{median:.0f} ns', horizontalalignment='left', verticalalignment='center', color='black')
+        plt.text(i+textoffset, median, f'{median:.2f} {unit}', horizontalalignment='left', verticalalignment='center', color='black')
     plt.ylim(ylimit)
     plt.xticks(rotation=45)
     plt.xlabel('Workload')
-    plt.ylabel('Latency [ns]')
+    plt.ylabel(f'Latency [{unit}]')
     plt.tight_layout()
     plt.savefig(plot_filename, format="pdf")
     plt.close()
@@ -36,7 +36,7 @@ files_no_vmi = [
     ('results/result_2024-12-22_01-03-58_no-vmi/read_byte.csv', 'read byte'),
     ('results/result_2024-12-22_01-03-58_no-vmi/read_page.csv', 'read page'),
 ]
-read_files_and_plot_box(files_no_vmi, "plots/execread_boxplot_no_vmi.pdf", (0, 10000), 0.28, (10, 6), 13)
+read_files_and_plot_box(files_no_vmi, "plots/execread_boxplot_no_vmi.pdf", (0, 10), 0.28, (10, 6), 13, 1000, "\u03bcs")
 
 files_vmi = [
     ('results/result_2024-12-22_01-04-11_vmi/timing_overhead.csv', 'timer'),
@@ -44,9 +44,9 @@ files_vmi = [
     ('results/result_2024-12-22_01-04-11_vmi/exec_page.csv', 'exec page'),
     ('results/result_2024-12-22_01-04-11_vmi/read_byte.csv', 'read byte'),
 ]
-read_files_and_plot_box(files_vmi, "plots/execread_boxplot_vmi.pdf", (0, 100000), 0.24, (10,6), 13)
+read_files_and_plot_box(files_vmi, "plots/execread_boxplot_vmi.pdf", (0, 100), 0.24, (10,6), 13, 1000, "\u03bcs")
 
 files_vmi_page = [
     ('results/result_2024-12-22_01-04-11_vmi/read_page.csv', 'read page'),
 ]
-read_files_and_plot_box(files_vmi_page, "plots/execread_boxplot_vmi_readpage.pdf", (20000000, 30000000), 0.1, (6, 6), 13)
+read_files_and_plot_box(files_vmi_page, "plots/execread_boxplot_vmi_readpage.pdf", (20, 30), 0.1, (6, 6), 13, 1000000, "ms")
